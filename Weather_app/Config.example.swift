83b5b6386d0bc189a5b78c4ba829b508137c2a68
Jaiveer <jaiveermatta1@gmail.com>
2025-09-27 17:50:54 -0230

    Key hidden and example config created

---
+++ Weather_app/Config.example.swift
@@ -0,0 +1,12 @@
+//
+//  config.example.swift
+//  Weather_app
+//
+//  Created by Jaiveer Matta on 2025-09-27.
+//
+
+import Foundation
+
+   struct config {
+       static let openWeatherMapAPIKey = "YOUR_API_KEY_HERE"
+   }
--- Weather_app/Managers/WeatherManager.swift
+++ Weather_app/Managers/WeatherManager.swift
@@ -9,8 +9,12 @@ import Foundation
 import CoreLocation
 
 class WeatherManager {
-    func getCurrentWeather(latitude:CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=96b78aa76af63e4ddce70e5a93dcd8e3&units=metric") else { fatalError("Missing URL")}
+    private let apiKey = Config.openWeatherMapAPIKey
+    
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric") else {
+            fatalError("Missing URL")
+        }
         
         let urlRequest = URLRequest(url: url)
         
