2c801a21b2d8cf3f7912ac81ad4e2fd11907009c
Jaiveer <jaiveermatta1@gmail.com>
2025-09-27 15:40:45 -0230

    Initial Commit

---
+++ Weather_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather_app
+//
+//  Created by Jaiveer Matta on 2025-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather_app/Weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_appApp.swift
+//  Weather_app
+//
+//  Created by Jaiveer Matta on 2025-09-27.
+//
+
+import SwiftUI
+
+@main
+struct Weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
