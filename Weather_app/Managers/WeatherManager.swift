54eb617b767f956fb47f1164fd80f5d42d2ce802
Jaiveer <jaiveermatta1@gmail.com>
2025-09-27 17:32:32 -0230

    Api call fixed

--- Weather_app/Managers/WeatherManager.swift
+++ Weather_app/Managers/WeatherManager.swift
@@ -10,7 +10,7 @@ import CoreLocation
 
 class WeatherManager {
     func getCurrentWeather(latitude:CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid={96b78aa76af63e4ddce70e5a93dcd8e3}&units=metric") else { fatalError("Missing URL")}
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=96b78aa76af63e4ddce70e5a93dcd8e3&units=metric") else { fatalError("Missing URL")}
         
         let urlRequest = URLRequest(url: url)
         
